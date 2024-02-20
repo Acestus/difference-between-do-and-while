eabca77e0a4ad52de2f7c3d81614a711f3221e98
wbalconi <wbalconi@3arrows.us>
2024-02-19 21:19:40 -0500

    Initial commit

---
+++ serenade.cs
@@ -0,0 +1,28 @@
+﻿string? readResult;
+bool validEntry = false;
+bool validNumber = false;
+Console.WriteLine("Give a value between 5 and 10:");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        validNumber = int.TryParse(readResult, out int result);
+        if (validNumber && result >= 5 && result <= 10)
+        {
+            Console.WriteLine($"Input: {result}");
+            validEntry = true;
+        }
+        else if (validNumber)
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 5 and 10.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+    }
+} while (validEntry == false);
+
+Console.WriteLine("Your input is valid. Thank you!");
